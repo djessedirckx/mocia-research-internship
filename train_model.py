92e2c534fac99438ea20378e002a06be7ba22a0f
djessedirckx <djessedirckx@gmail.com>
2021-10-08 11:43:32 +0200

    Added validation score + metrics to training loop

--- train_model.py
+++ train_model.py
@@ -1,4 +1,5 @@
 import itertools
+from typing import Tuple
 import numpy as np
 import tensorflow as tf
 
@@ -7,6 +8,7 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.losses import CategoricalCrossentropy
 from tensorflow.keras.utils import Progbar
 from sklearn.model_selection import StratifiedKFold, train_test_split
+from sklearn.metrics import roc_auc_score, average_precision_score
 
 from eda_preprocessing.DataPreprocessor import DataPreprocessor
 from eda_preprocessing.TrainingDataCreator import TrainingDataCreator
@@ -59,10 +61,14 @@ def train(folds, epochs: int, batch_size: int):
         train_windows = np.array(list(itertools.chain.from_iterable(feature_windows[train_idx])))
         train_masks = np.array(list(itertools.chain.from_iterable(mask_windows[train_idx])))
 
-        # val_measurement_labels = measurement_labels[val_idx]
+        # Unpack validation data from nested list
+        val_measurement_labels = np.array(list(itertools.chain.from_iterable(measurement_labels[val_idx])))
+        val_windows = np.array(list(itertools.chain.from_iterable(feature_windows[val_idx])))
+        val_masks = np.array(list(itertools.chain.from_iterable(mask_windows[val_idx])))
 
         # Define array for retrieving feature windows and masks during training
         train_batch_idx = np.arange(0, len(train_windows))
+        val_idx = np.arange(0, len(val_windows))
 
         # Split training data into minibatches
         train_dataset = tf.data.Dataset.from_tensor_slices((train_batch_idx, train_measurement_labels))
@@ -71,11 +77,15 @@ def train(folds, epochs: int, batch_size: int):
         # Create the model
         model = build_model(model_config)
 
+        # Training stats
+        best_val_loss = np.inf
+        best_model_weights = None
+
         # Iterate for the specified number of epochs
         for epoch in range(epochs):
             print(f'epoch: {epoch+1}/{epochs} - fold: {fold_step+1}/{folds}\n')
 
-            prog_bar = Progbar(target=len(train_batch_idx), stateful_metrics=['train_loss'])
+            prog_bar = Progbar(target=len(train_batch_idx), stateful_metrics=['train_loss', 'AUROC', 'AUPRC'])
 
             # Iterate over the minibatches
             for step, (train_batch_data, train_batch_labels) in enumerate(train_dataset):
@@ -86,36 +96,62 @@ def train(folds, epochs: int, batch_size: int):
 
                 with tf.GradientTape() as tape:
                     predictions = model([windows, masks], training=True)
-
-                    loss = []
-                    for i in range(len(predictions)):
-                        prediction = predictions[i]
-                        label = train_batch_labels[:, i]
-
-                        loss.append(loss_fn(label, prediction))
-
-                    loss = tf.concat(loss, 0)
-                    loss = tf.reduce_mean(loss)
-
-                    # for prediction, label in zip(predictions, train_batch_labels):
-                    #     loss = loss_fn(label, prediction)
-
-                    # print(len(predictions))
-                    # loss = loss_fn(train_batch_labels, predictions)
-                    # loss_idx = tf.math.is_nan(loss)
-                    # print(train_batch_labels)
-                    # print(predictions)
-                    # print(loss)
-                    # loss = loss[~loss_idx]
-                    # # loss = tf.reduce_mean(loss)
+                    loss = compute_loss(loss_fn, train_batch_labels, predictions)
 
                 gradients = tape.gradient(loss, model.trainable_weights)
                 optimizer.apply_gradients(zip(gradients, model.trainable_weights))
 
+                # Compute metrics
+                au_roc, au_prc = compute_metrics(train_batch_labels, predictions)
+                if au_roc != 0 and au_prc != 0:
+                    prog_metrics = [('train_loss', loss), ('AUROC', au_roc), ('AUPRC', au_prc)]
+                else:
+                    prog_metrics = [('train_loss', loss)]
+
                 # Update progress bar
-                prog_metrics = [('train_loss', loss)]
                 prog_bar.add(batch_size, values=prog_metrics)
 
+            if epoch % 10 == 0:
+                # Compute validation performance
+                val_predictions = model([val_windows, val_masks], training=False)
+
+                val_loss = compute_loss(loss_fn, val_measurement_labels, val_predictions)
+                val_au_roc, val_au_prc = compute_metrics(val_measurement_labels, val_predictions)
+
+                print(f'Validation loss: {val_loss}, AUROC: {val_au_roc}, AUPRC: {val_au_prc}')
+
+                if val_loss < best_val_loss:
+                    best_val_loss = val_loss
+                    best_model_weights = model.get_weights()
+
+def compute_loss(loss_fn, labels, predictions):
+    loss = []
+    
+    for i in range(len(predictions)):
+        prediction = predictions[i]
+        label = labels[:, i]
+        loss.append(loss_fn(label, prediction))
+
+    loss = tf.concat(loss, 0)
+    return tf.reduce_mean(loss)
+
+def compute_metrics(labels, predictions) -> Tuple[int, int]:
+    au_roc, au_rpc, decay = 0, 0, 0
+    for i in range(len(predictions)):
+        prediction = predictions[i]
+        label = labels[:, i]
+
+        # Compute AUROC and AURPC score. Try except is added in case only one class is present
+        # for the respective timepoint in the minibatch
+        try:
+            au_roc += roc_auc_score(label, prediction)
+            au_rpc += average_precision_score(label, prediction)
+        except:
+            decay += 1
+
+    if len(predictions) - decay != 0:
+        return au_roc / (len(predictions) - decay), au_rpc / (len(predictions) - decay)
+    return 0, 0
 
 if __name__ == '__main__':
     folds = 5
